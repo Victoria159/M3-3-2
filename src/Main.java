d5a4cba73919a5cc965f9e62afb5eac2ba1fbfa4
Victoria Bocharova <v.s.bocha@gmail.com>
2021-07-17 21:36:41 +0300

    Changed Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         int startAmount = 100;
-        int depositAmount = 1150;
+        int depositAmount = 2500;
 
-        int bonus;
-        int totalAmount;
+        int bonus = depositAmount / 100;
+        int totalAmount = bonus + startAmount + depositAmount;
         if (depositAmount > 1000) {
-            bonus = depositAmount / 100;
-            totalAmount = bonus + startAmount + depositAmount;
             System.out.println(bonus);
             System.out.println(totalAmount);
         } else{
-            totalAmount = startAmount + depositAmount;
             System.out.println(totalAmount);
         }
         }
